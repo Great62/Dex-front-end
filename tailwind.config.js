bd6c8e102c925ab531bb0fee2a52665f47a4fb8f
Adam <Adamkindel1234@gmail.com>
2022-05-29 22:51:55 +0200

    css fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
   theme: {
     extend: {},
+    fontFamily: {
+      custom: ['Montserrat', 'sans-serif'],
+    },
   },
   plugins: [],
 }
